1e6119ff95d4c6253355a88f979fa16b8ed39e36
DESKTOP-IANPIRQ\qq_18 <qq18929261343@gmail.com>
2026-07-11 12:43:22 +0800

    Change api call due to mis-implementation behaviour in Escape in Duckov

--- FastModdingLib/Saves/SaveUtils.cs
+++ FastModdingLib/Saves/SaveUtils.cs
@@ -9,8 +9,8 @@ public class SaveUtils
     public static T? Load<T>(Identifier identifierKey)
     {
         return
-            SavesSystem.KeyExisits(ModBehaviour.FrameworkName, identifierKey.ToString()) ?
-                SavesSystem.Load<T>(ModBehaviour.FrameworkName, identifierKey.ToString()) :
+            SavesSystem.KeyExisits(ModBehaviour.FrameworkName + identifierKey) ?
+                SavesSystem.Load<T>(ModBehaviour.FrameworkName + identifierKey) :
                 default;
     }
 
@@ -18,13 +18,13 @@ public class SaveUtils
     {
         if (value == null)
         {
-            if (SavesSystem.KeyExisits(ModBehaviour.FrameworkName, identifierKey.ToString()))
+            if (SavesSystem.KeyExisits(ModBehaviour.FrameworkName + identifierKey))
             {
                 ES3.DeleteKey(ModBehaviour.FrameworkName + identifierKey, SavesSystem.CurrentFilePath);
             }
             return;
         }
 
-        SavesSystem.Save(ModBehaviour.FrameworkName, identifierKey.ToString(), value);
+        SavesSystem.Save<T>(ModBehaviour.FrameworkName + identifierKey, (T) value);
     }
 }
